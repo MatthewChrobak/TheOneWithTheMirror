decd6b9ced1cea6158e5a1092cfea93ad98eaa59
Matthew Chrobak <MatthewChrobak@hotmail.com>
2019-11-23 14:12:14 -0500

    Done task

--- Game/Game/Models/Chunks/Map.cs
+++ Game/Game/Models/Chunks/Map.cs
@@ -12,15 +12,19 @@ namespace Game.Models.Chunks
         }
 
         public MapChunk GetChunk(int x, int y) {
-            if (!this._chunks.ContainsKey((x, y))) {
-                this._chunks[(x, y)] = new MapChunk(x, y);
-            }
+            LoadChunk(x, y);
             return this._chunks[(x, y)];
         }
 
         public void Draw(ICanvas canvas) {
             foreach (var value in this._chunks.Values) {
                 value.Draw(canvas);
+            }
+        }
+
+        public void LoadChunk(int x, int y) {
+            if (!this._chunks.ContainsKey((x, y))) {
+                this._chunks[(x, y)] = new MapChunk(x, y);
             }
         }
     }
