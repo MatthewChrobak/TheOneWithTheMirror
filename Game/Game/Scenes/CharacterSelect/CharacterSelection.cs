f55305290086bac20d0ee6b8a5cd8268d831f6b6
Chun <lau_tsechun@hotmail.com>
2019-11-23 21:20:40 -0500

    Commit

---
+++ Game/Game/Scenes/CharacterSelect/CharacterSelection.cs
@@ -0,0 +1,117 @@
+﻿using Annex.Data.Shared;
+using Annex.Events;
+using Annex.Graphics;
+using Annex.Graphics.Contexts;
+using Annex.Graphics.Events;
+using Annex.Scenes;
+using Annex.Scenes.Components;
+using Game.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Scenes.CharacterSelect
+{
+    class CharacterSelection : Scene
+    {
+        private float controllerDeadzone = 50f;
+        private SpriteSheetContext[] characters = new SpriteSheetContext[4];
+        
+        private int index = 0;
+        public SpriteSheetContext selectedCharacter;        
+
+        public Player EditingPlayer;
+
+        public CharacterSelection()
+        {
+            for (int i = 0; i < characters.Length; i++)
+            {
+                characters[i] = new SpriteSheetContext("smushy.png", 1, 8)
+                {
+                    //RenderPosition = Vector.Create(GameWindow.RESOLUTION_WIDTH/2, GameWindow.RESOLUTION_HEIGHT/2),
+                    RenderSize = Vector.Create(25, 50)
+                };
+            }
+            characters[1] = new SpriteSheetContext("player.png", 4, 4)
+            {
+                //RenderPosition = Vector.Create(GameWindow.RESOLUTION_WIDTH / 2, GameWindow.RESOLUTION_HEIGHT / 2),
+                RenderSize = Vector.Create(96, 96)
+            };
+
+            characters[2] = new SpriteSheetContext("Clawdia_FacingUpUp.png", 1, 1)
+            {
+                //RenderPosition = Vector.Create(GameWindow.RESOLUTION_WIDTH / 2, GameWindow.RESOLUTION_HEIGHT / 2),
+                RenderSize = Vector.Create(32, 32)
+            };
+
+            characters[3] = new SpriteSheetContext("Clawdia_DirectionSheet.png", 3, 3)
+            {
+                RenderSize = Vector.Create(32, 32)
+        };
+            
+            this.Events.AddEvent("ChooseCharacter", PriorityType.INPUT, ChooseCharacter, 500);
+        }
+
+        public ControlEvent ChooseCharacter()
+        {
+            var canvas = GameWindow.Singleton.Canvas;            
+
+            var characterNotification = this.GetElementById(CharacterSelect.Elements.CharacterSelect.ID);
+            if (characterNotification == null)
+            {
+                this.AddChild(new CharacterSelect.Elements.CharacterSelect());
+            }
+            
+            var dx = canvas.GetJoystickAxis(EditingPlayer._joystickID, JoystickAxis.X);
+
+            if (dx > 0 + controllerDeadzone)
+            {
+                index = (index + 1) % characters.Length;
+
+            }
+            else if (dx < 0 - controllerDeadzone)
+            {
+                index = (index + characters.Length - 1) % characters.Length;
+            }            
+
+            return ControlEvent.NONE;
+        }        
+
+        public override void HandleCloseButtonPressed()
+        {
+            SceneManager.Singleton.LoadScene<GameClosing>();
+        }
+
+        public override void HandleJoystickButtonPressed(JoystickButtonPressedEvent e)
+        {
+            if (e.Button == JoystickButton.A)
+            {
+                selectedCharacter = characters[index];
+                EditingPlayer._sprite = new SpriteSheetContext(selectedCharacter.SourceTextureName, (uint)selectedCharacter.NumRows, (uint)selectedCharacter.NumColumns)
+                {
+                    RenderPosition = EditingPlayer.Position,
+                    RenderSize = Vector.Create(25, 50)
+                };
+                SceneManager.Singleton.LoadScene<Stage1.Stage1>();
+            }
+            if (e.Button == JoystickButton.Back)
+            {
+                this.HandleCloseButtonPressed();
+            }
+        }
+
+        public override void HandleKeyboardKeyPressed(KeyboardKeyPressedEvent e) {
+            if (e.Key == KeyboardKey.Space) {
+                this.HandleJoystickButtonPressed(new JoystickButtonPressedEvent() {
+                    Button = JoystickButton.A
+                });
+            }
+        }
+
+        public override void Draw(ICanvas canvas)
+        {
+            canvas.Draw(characters[index]);                
+            base.Draw(canvas);
+        }
+    }
+}
---
+++ Game/Game/Scenes/CharacterSelect/Elements/CharacterSelect.cs
@@ -0,0 +1,25 @@
+﻿using Annex.Data;
+using Annex.Graphics;
+using Annex.Scenes.Components;
+
+namespace Game.Scenes.CharacterSelect.Elements
+{
+    class CharacterSelect : Label
+    {
+        public const string ID = "lblCharacterSelect";
+
+        public CharacterSelect() : base(ID)
+        {
+            this.Caption.Set("SELECT YOUR CHARACTER");
+            this.Font.Set("default.ttf");
+            this.Position.Set(0, 0);
+            this.FontSize.Set(24);
+            this.Size.Set(GameWindow.RESOLUTION_WIDTH, GameWindow.RESOLUTION_HEIGHT);
+            this.RenderText.Alignment.VerticalAlignment = VerticalAlignment.Top;
+            this.RenderText.Alignment.HorizontalAlignment = HorizontalAlignment.Center;
+            this.RenderText.FontColor = RGBA.White;
+            this.RenderText.BorderColor = RGBA.Black;
+            this.RenderText.BorderThickness = 2;
+        }
+    }
+}
